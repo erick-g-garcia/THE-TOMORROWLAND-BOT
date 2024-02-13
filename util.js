ab8a29f4ca0ea113735a9fb4b95eefcc77757274
erick-g-garcia <159676190+erick-g-garcia@users.noreply.github.com>
2024-02-13 16:39:50 -0600

    Update util.js

--- util.js
+++ util.js
@@ -1,17 +1,12 @@
 export default {
   phoneList: function (list) {
-    return list.map((item) => {
-      const contact = client.getContactById(item); // Assuming client.getContactById() is a function to get contact details by ID
-      const phoneNumber = item.replace('@c.us', '');
-      const user = contact ? `@${contact.id.user}` : ''; // Get user if contact exists
-      return `${user} (${phoneNumber})`;
-    }).join(', ');
+    return list.map((item) => item.replace('@c.us', '')).join(', ')
   },
 
   karmaList: function (list) {
     var items = []
     Object.keys(list).forEach((phone) => {
-      items.push(`${phone.replace('@c.us', '')}: ${list[phone]}`)
+      items.push(`${phone.replace('@c.us', '')}: @${contact.id.user} `)
     })
 
     return items.join('\n')

baa41170fc49d1c7bfd598c1bb82558d5fc716fd
erick-g-garcia <159676190+erick-g-garcia@users.noreply.github.com>
2024-02-13 15:48:48 -0600

    Update util.js
    intneto de contacto una vez mas

--- util.js
+++ util.js
@@ -1,16 +1,22 @@
 export default {
+  phoneListWithContactId: function (list) {
+    return list.map((item) => {
+      const [phone, contactId] = item.split('@c.us');
+      return { phone: phone.trim(), contactId: contactId.trim() };
+    });
+  },
+
   phoneList: function (list) {
-    return list.map((item) => item.replace('@c.us', '')).join(', ')
+    return list.map(item => item.phone).join(', ');
   },
 
   karmaList: function (list) {
-    var items = []
+    var items = [];
     Object.keys(list).forEach((phone) => {
-      items.push(`${phone.replace('@c.us', '')}: ${list[phone]}`)
-    })
+      items.push(`${phone.replace('@c.us', '')}: ${list[phone]}`);
+    });
 
-    return items.join('\n')
+    return items.join('\n');
   },
-}
-
+};
 

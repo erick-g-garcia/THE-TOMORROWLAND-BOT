0982e236e4ffab44a52b533ce89c20c2afd61d3e
erick-g-garcia <159676190+erick-g-garcia@users.noreply.github.com>
2024-02-13 12:49:45 -0600

    Update util.js

--- util.js
+++ util.js
@@ -1,16 +1,27 @@
 export default {
-  phoneList: function (list) {
-    return list.map((item) => item.replace('@c.us', '')).join(', ')
+  phoneList: function (list, userData) {
+    const formattedList = list.map((item) => {
+      const user = userData[item];
+      if (user) {
+        return `${item} (@${user})`;
+      } else {
+        return item;
+      }
+    });
+    return formattedList.join(', ');
   },
 
-  karmaList: function (list) {
+  karmaList: function (list, userData) {
     var items = []
     Object.keys(list).forEach((phone) => {
-      items.push(`${phone.replace('@c.us', '')}: ${list[phone]}`)
-    })
+      const user = userData[phone];
+      if (user) {
+        items.push(`@${user}: ${list[phone]}`);
+      } else {
+        items.push(`${phone}: ${list[phone]}`);
+      }
+    });
 
-    return items.join('\n')
-  },
-}
-
-
+    return items.join('\n');
+  }
+};

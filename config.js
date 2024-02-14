714807f12ff534de64297679ae485a995c91ad05
erick-g-garcia <159676190+erick-g-garcia@users.noreply.github.com>
2024-02-13 19:33:38 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 export default {
   modRoom: '120363045998091546@g.us',
   karma: {},
+  rooms: [],
   testrooms: ['120363211157430528@g.us','120363228613842697@g.us','120363219738969843@g.us'
   ],
   blacklist: [

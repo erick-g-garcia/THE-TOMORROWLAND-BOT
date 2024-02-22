b96bc3285aab3a879afcdab8ebfa132c061271a4
erick-g-garcia <159676190+erick-g-garcia@users.noreply.github.com>
2024-02-22 06:34:37 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -486,13 +486,9 @@ If you think it is a mistake, send a message to an admin to clear your karma lev
   }
 });
 
-// Instead of declaring config again, you can directly assign values to it
-config = {};
+
 
 // Rest of your code here...
-
-client.on('message', async (message) => {
-  console.log('Received message:', message);
 
   if (message.body.toLowerCase() === '!cleanrecovery') {
     try {
